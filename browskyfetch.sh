fdc33afa7bbf41ecc9e725ae954e4963d8d17734
browsky300 <browsky300@gmail.com>
2022-08-26 04:15:45 -0700

    remove some stuff its 4 am and im tired

--- browskyfetch.sh
+++ browskyfetch.sh
@@ -12,8 +12,4 @@ finurl=${finurl%%\/}
 echo "Welcome to $finurl, $(whoami)!"
 echo 
 echo "Using the $(uname -s) kernel on version $(uname -r)"
-echo
-echo "Host: $(cat /sys/class/dmi/id/sys_vendor) $(cat /sys/class/dmi/id/product_version)"
-echo
-echo "Shell: $(basename $SHELL)"
 echo -e "\e[0;30m██\e[0;31m██\e[0;32m██\e[0;33m██\e[0;34m██\e[0;35m██\e[0;36m██\e[0;37m██\n\e[1;30m██\e[1;31m██\e[1;32m██\e[1;33m██\e[1;34m██\e[1;35m██\e[1;36m██\e[1;37m██\e[0m"
